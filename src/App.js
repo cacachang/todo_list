c5c6348c2a57c748696a7b2664c92456046a1eaa
Ning <a24701770@gmail.com>
2024-04-05 23:42:45 +0800

    add: use useState to create and delete and check function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 } from "uuid";
+import "./App.css";
 
 function App() {
+  const [inputValue, setValue] = useState("");
+  const [lists, setLists] = useState([]);
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setLists((currentValue) => [
+      ...currentValue,
+      { id: v4(), title: inputValue, isCompleted: false }
+    ]);
+    setValue("");
+  };
+
+  const handleDelete = (id) => {
+    setLists((currentValue) => currentValue.filter((li) => li.id !== id));
+  };
+
+  const handleCheck = (id, target) => {
+    setLists((currentValue) =>
+      currentValue.map((li) => {
+        if (id == li.id) {
+          return { ...li, isCompleted: target.checked };
+        } else {
+          return { ...li };
+        }
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={(e) => handleInput(e)} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New ITEM</label>
+          <input
+            type="text"
+            id="item"
+            onChange={(e) => setValue(e.target.value)}
+            value={inputValue}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header"> TODO LIST</h1>
+      <ul className="list">
+        {lists &&
+          lists.length !== 0 &&
+          lists.map((list) => {
+            return (
+              <li key={list.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={list.isCompleted}
+                    onChange={(e) => handleCheck(list.id, e.target)}
+                  />
+                  {list.title}
+                </label>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(list.id)}
+                >
+                  DELETE
+                </button>
+              </li>
+            );
+          })}
+      </ul>
+    </>
   );
 }
 
